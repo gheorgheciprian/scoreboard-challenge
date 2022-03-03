c0ffe18586898272ee4cdc3003fcbc807447339d
William Taylor <williamtaylor@LT4235-William-Taylor.local>
2022-03-03 18:16:15 +0000

    test: update to include more tennis scores

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Times Higher Education JS Challenge 
+# Times Higher Education JS Challenge
 
 This is the take home part of the Times Higher Education JavaScript challenge
 where your goal is to fix all of the tests found in `scoreboard.test.js`.
@@ -6,6 +6,8 @@ where your goal is to fix all of the tests found in `scoreboard.test.js`.
 Please bring this challenge with you for your video interview where we will pair
 on expanding the functionality. Also, please refrain from hosting your challenge
 anywhere public.
+
+note: please do not edit `scoreboard.test.js` or use the data in `testDataOnly.js` for your solution.
 
 ## Setup
 
---
+++ src/testDataOnly.js
@@ -0,0 +1,38 @@
+export const scores = [
+  //draws
+  [0, 0, "Love-All"],
+  [1, 1, "15-All"],
+  [2, 2, "30-All"],
+  [3, 3, "Deuce"],
+  [4, 4, "Deuce"],
+  [5, 5, "Deuce"],
+  //player1
+  [1, 0, "15-Love"],
+  [2, 0, "30-Love"],
+  [3, 0, "40-Love"],
+  [2, 1, "30-15"],
+  [3, 1, "40-15"],
+  [3, 2, "40-30"],
+  //player2
+  [0, 1, "Love-15"],
+  [0, 2, "Love-30"],
+  [0, 3, "Love-40"],
+  [1, 2, "15-30"],
+  [1, 3, "15-40"],
+  [2, 3, "30-40"],
+  //winning scores
+  [4, 0, "Game, player1"],
+  [0, 4, "Game, player2"],
+  [4, 6, "Game, player2"],
+  [6, 4, "Game, player1"],
+  //advantage p1
+  [5, 4, "Advantage, player1"],
+  [6, 5, "Advantage, player1"],
+  [13, 12, "Advantage, player1"],
+  [99, 98, "Advantage, player1"],
+  //advantage p2
+  [4, 5, "Advantage, player2"],
+  [5, 6, "Advantage, player2"],
+  [12, 13, "Advantage, player2"],
+  [98, 99, "Advantage, player2"],
+];
